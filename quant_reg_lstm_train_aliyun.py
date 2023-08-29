9237363c10a6edce35ffcf09bc7799b166a02980
teancake <teancake.github@gmail.com>
2023-08-29 14:34:55 +0800

    test

--- quant_reg_lstm_train_aliyun.py
+++ quant_reg_lstm_train_aliyun.py
@@ -5,10 +5,6 @@ from torch.utils.data import DataLoader, TensorDataset
 from datetime import datetime, timedelta
 import torch.nn as nn
 
-import uuid
-import sys
-from concurrent.futures import ProcessPoolExecutor, wait
-from tqdm import tqdm
 import pickle
 
 import os
@@ -69,62 +65,6 @@ def load_data_from_file(file_path):
     return data
 
 
-def _get_sequential_data_nparray(symbol_df, sequence_length, number_of_sequences):
-    list_x = []
-    list_y = []
-    list_ext = []
-    # print("symbol {}, number of dates {}, min dates {}".format(symbol, symbol_df.shape[0], symbol_df["日期"].min()))
-    for i in range(symbol_df.shape[0] - sequence_length + 1):
-        date_df = symbol_df[i:i + sequence_length]
-        # print("i {}, len {}, min date {}".format(i, date_df.shape, date_df["日期"].min()))
-        x = date_df.loc[:, "ma_5":"turnover_rate"].fillna(0).values.astype(float)
-        y = date_df.loc[:, "label"].values.astype(float)[0]
-        ext = date_df.loc[:, ["日期", "代码", "label"]]
-        ext = ext.iloc[[0]]
-
-        list_x.append(x)
-        list_y.append(y)
-        list_ext.append(ext)
-        if number_of_sequences is not None and i >= number_of_sequences - 1:
-            break
-    return list_x, list_y, list_ext
-
-
-def get_sequential_data(df, sequence_length=1, number_of_sequences=None):
-    POOL_SIZE = 16
-    pool = ProcessPoolExecutor(max_workers=POOL_SIZE)
-    data_x = []
-    data_y = []
-    data_ext = []
-    futures = []
-    for symbol in tqdm(df["代码"].unique()):
-        symbol_df = df[df["代码"] == symbol].sort_values(by=["日期"], ascending=False)
-        future = pool.submit(_get_sequential_data_nparray, symbol_df, sequence_length, number_of_sequences)
-        futures.append(future)
-        if len(futures) % POOL_SIZE == POOL_SIZE - 1:
-            # the main purpose of waiting is to make tqdm correct.
-            wait(futures)
-        if symbol > "000070":
-            break
-
-    wait(futures)
-
-    for future in futures:
-        list_x, list_y, list_ext = future.result()
-        data_x.extend(list_x)
-        data_y.extend(list_y)
-        data_ext.extend(list_ext)
-
-    pool.shutdown(wait=True)
-
-    data_x = np.stack(data_x, axis=0)
-    data_y = np.stack(data_y, axis=0)
-    data_ext = pd.concat(data_ext, ignore_index=True, sort=False)
-
-    print("sequential data x shape {}, sequential data y shape {}, ext data shape {}".format(data_x.shape, data_y.shape, data_ext.shape))
-    return data_x, data_y, data_ext
-
-
 
 
 def train(args):
@@ -135,8 +75,9 @@ def train(args):
     drop_prob = args.drop_prob
     hidden_dim = args.hidden_dim
     epoch_num = args.epoch_num
+    data_file_name = args.data_file_name
 
-    sequential_data = load_data_from_file("/mnt/data/quant_reg_sequential_data.pkl")
+    sequential_data = load_data_from_file("/mnt/data/{}".format(data_file_name))
     train_data_x, train_data_y, test_data_x, test_data_y, test_data_ext, pred_data_x, pred_data_ext = sequential_data
 
     pred_data_x = pred_data_x[0:2,:,:]
@@ -256,6 +197,7 @@ if __name__ == "__main__":
     parser.add_argument('--n_layers', type=int, default=1, help='lstm number of layers')
     parser.add_argument('--drop_prob', type=float, default=0.2, help='lstm dropout probability')
     parser.add_argument('--hidden_dim', type=int, default=512, help='lstm hidden variable dimension')
+    parser.add_argument('--data_file_name', type=int, default="quant_reg_sequential_data.pkl", help='data file name')
 
     args = parser.parse_args()
     print("arguments {}".format(args))
